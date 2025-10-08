f911280690913d1225fa3b290efcb1d906dade19
LeoQC13 <quiroscollinaleo@gmail.com>
2025-10-07 20:27:29 -0600

    agregar archivo Leo.java, pidiendo número de teléfono

---
+++ src/javaapplication9/Leo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication9;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author quiro
+ */
+public class Leo {
+    public static void main(String[] args) {
+        System.out.println("Ingrese su ID ");
+        Scanner dato = new Scanner (System.in);
+        String ID = dato.nextLine();
+        
+        System.out.println("Ingrese su ID: " + ID);
+    }
+    
+    
+}
